8e3b2f55787a04cbcf19f9411f8d0a04673d6ed5
Akankshya-96 <akankshyamishra.96@gmail.com>
2021-04-26 03:23:54 -0500

    checkout option in cart.. completed

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from .models import Account
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from carts.views import _cart_id
+from carts.models import Cart,CartItem
+import requests
 # Create your views here.
 #verification email code
 from django.contrib.sites.shortcuts import get_current_site
@@ -46,7 +49,6 @@ def register(request):
     'form':form,
     }
     return render(request,'accounts/register.html', context)
-
 def login(request):
     if request.method == 'POST':
         email = request.POST['email']
@@ -55,20 +57,64 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart=Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    # Getting product variation by cart_id
+                    product_variation=[]
+                    for item in cart_item:
+                        variation=item.variations.all()
+                        product_variation.append(list(variation))
+                    # get the cart items from the user to access product variation
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list=[]
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+                    # product_variation =[1,2,3,4]
+                    # ex_var_list= [4,5,6]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.Quantity +=1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item =CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+
+            except:
+                pass
             auth.login(request, user)
             messages.success(request,'You are now logged In.')
-            return redirect('dashboard')
+            url =request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request,'Invalid login credentials')
             return redirect('login')
     return render(request,'accounts/login.html')
-
 @login_required(login_url = 'login')
 def logout(request):
     auth.logout(request)
     messages.success(request,'You are logged out.')
     return redirect('login')
-
 def activate(request,uidb64,token):
     try:
         uid=urlsafe_base64_decode(uidb64).decode()
@@ -83,11 +129,9 @@ def activate(request,uidb64,token):
     else:
         messages.error(request,'Invalid Activation Link')
         return redirect('register')
-
 @login_required(login_url = 'login')
 def dashboard(request):
     return render(request,'accounts/dashboard.html')
-
 def forgotPassword(request):
     if request.method =='POST':
         email = request.POST['email']
@@ -111,7 +155,6 @@ def forgotPassword(request):
             messages.error(request,'Account Does Not Exist!')
             return redirect('forgotPassword')
     return render(request,'accounts/forgotPassword.html')
-
 def reset_password_validate(request,uidb64,token):
     try:
         uid=urlsafe_base64_decode(uidb64).decode()
