0fca48c33633f2a8863af1ed6c907c9730f76e75
Akankshya-96 <akankshyamishra.96@gmail.com>
2021-05-15 05:59:45 -0500

    Updated Product Image

--- accounts/views.py
+++ accounts/views.py
@@ -16,7 +16,7 @@ from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
-###################
+#########################################################################
 def register(request):
     if request.method == 'POST':
         form = RegisterationForm(request.POST)
